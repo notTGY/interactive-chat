cc9f97fa5aabfd1d4e3247f6b309602e846f1e38
notTGY <microcraft47@gmail.com>
2025-09-10 10:23:17 +0300

    add more videos

--- __main__.py
+++ __main__.py
@@ -11,12 +11,15 @@ load_dotenv()
 TOKEN = os.getenv('BOT_TOKEN')
 if not TOKEN:
     raise ValueError("BOT_TOKEN not found in environment variables")
-CHAT_ID = -1002678420298
-MESSAGE_ID = 15
+
+# No seed wasted
+# CHAT_ID = -1002678420298
+# MESSAGE_ID = 15
 
 
-# CHAT_ID = -1002171080268
-# MESSAGE_ID = 304
+# Fear or Love
+CHAT_ID = -1002171080268
+MESSAGE_ID = 304
 
 VIDEO_DIR = 'videos/'  # Folder with MP4s (e.g., vid1.mp4, vid2.mp4)
 VIDEO_FILES = [f for f in os.listdir(VIDEO_DIR) if f.endswith('.mp4')]
@@ -25,7 +28,14 @@ if not VIDEO_FILES:
 
 minutes_between_updates = 1
 
-caption = "The cycle continues..."
+caption = """Бесконечный brainrot
+
+Это сообщение своего рода quine. Телеграм бот каждую минуту редактирует это сообщение целиком и меняет видео.
+
+В статическом мире телеграма этот пост "особенный".
+
+github.com/nottgy/interactive-chat"""
+
 prev_video = ''
 
 async def update_post(bot):
